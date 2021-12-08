1bd7271e5ee10d59e4604f9ae75bb3a7c5a1233f
TriangleDevv <chandu.lanka13@gmail.com>
2021-12-08 17:24:17 +0530

    fixed some bugs

--- conf.lua
+++ conf.lua
@@ -1,7 +1,6 @@
 function love.conf(t)
     t.title = "Button Clicker"
     t.version = "11.3"
-    t.console = true
     t.width = 600
     t.height = 600
     t.vsync = 0
--- main.lua
+++ main.lua
@@ -5,9 +5,11 @@ function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.mouse.setVisible(false)
 
+    number = 0
+
     font = "data/fonts/m5x7.ttf"
     button1 = simple_button:create("rect", "fill", 300, 100, 200, 100, 255, 255, 255, 255, 0, 255, 1)
-    button2 = text_button:create("rect", "fill", "Hello", 370, 330, font, 45, 300, 300, 200, 100, 255, 255, 255, 255, 0, 255, 1)
+    button2 = text_button:create("rect", "fill", "Hello", 370, 330, font, 45, 300, 300, 200, 100, 255, 255, 255, 255, 255, 0, 1)
 end
 
 function love.update(dt)
@@ -15,17 +17,17 @@ function love.update(dt)
     text_button:update(dt)
 end
 
-function hello()
-    print("u clicked the button also hello")
+function increment_1()
+    number = number + 1
 end
 
 function love.mousepressed(x, y, button, istouch)
     if simple_button:onpressed(button) then
-        hello()
+        increment_1()
     end
 
     if text_button:onpressed(button) then
-        hello()
+        increment_1()
     end
 end
 
@@ -38,4 +40,6 @@ function love.draw()
     
     love.graphics.setColor(255,255,255)
     love.graphics.print("hello world", 0, 0)
+    love.graphics.print(number, 300, 500)
+
 end
