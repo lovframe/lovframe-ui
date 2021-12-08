a421220591be67eefa7520ffcaec73fed4954303
TriangleDevv <chandu.lanka13@gmail.com>
2021-12-08 07:01:49 +0530

    going to work on the text_button next which will have text init

---
+++ data/scripts/text_button.lua
@@ -0,0 +1,59 @@
+text_button = {}
+local hovered = false
+
+function text_button:create(type, text, text_x, text_y, mode, x, y, width, height, r, g, b, h_r, h_g, h_b, button)
+    self.type = type
+    self.mode = mode
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.r = r
+    self.g = g
+    self.b = b
+    self.h_r = h_r
+    self.h_g = h_g
+    self.h_b = h_b
+    self.button = button
+
+    -- text
+    self.text = text
+    self.text_x = text_x
+    self.text_y = text_y
+end
+
+function text_button:update(dt)
+    mouse_x, mouse_y = love.mouse.getPosition()
+
+    if self.type == "rect" then
+        if mouse_x > self.x and mouse_x < self.x + self.width then
+            if mouse_y > self.y and mouse_y < self.y + self.height then
+                hovered = true
+                return hovered
+            end
+        else
+            hovered = false
+        end
+    end
+end
+
+function text_button:onpressed(button)
+    if button == 1 and mouse_x > self.x and mouse_x < self.x + self.width then
+        if mouse_y > self.y and mouse_y < self.y + self.height then
+            return true
+        end
+    else
+        return false
+    end
+end
+
+function text_button:draw()
+    if self.type == "rect" then
+        if hovered == true then 
+            love.graphics.setColor(self.h_r, self.h_g, self.h_b, 255)
+        else
+            love.graphics.setColor(self.r, self.g, self.b, 255)
+        end
+        love.graphics.rectangle(self.mode, self.x, self.y, self.width, self.height)
+    end
+end
